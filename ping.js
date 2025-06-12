117349d92f9e4350302120d3bda31b1dc83b7b48
gifteddaves <comradeanasafa@gmail.com>
2025-06-12 04:58:29 +0300

    Delete ping.js

--- ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "ping",
-  description: "Ping the bot",
-  execute: async (sock, msg) => {
-    const jid = msg.key.remoteJid;
-    await sock.sendMessage(jid, { text: "Pong! 🏓" });
-  },
-};
