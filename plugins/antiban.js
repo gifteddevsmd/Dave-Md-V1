0681d728c2700b81bfc5d33e75a6dd5960627bca
gifteddaves <comradeanasafa@gmail.com>
2025-06-12 07:10:02 +0300

    Delete plugins/antiban.js

--- plugins/antiban.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'antiban',
-  description: 'Minimizes risk of WhatsApp bans (passive protection)',
-  execute: async (sock, msg) => {
-    // This acts as a passive plugin, maybe logs or prevents spamming
-    console.log('🛡️ Anti-ban active: avoiding risky actions...');
-  }
-};
