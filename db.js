d4a5419b5d9adcdf3c927b3920e67a7fe2443ad9
gifteddaves <comradeanasafa@gmail.com>
2025-06-13 17:18:50 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,21 +1,20 @@
-const { Low } = require('lowdb')
-const { JSONFile } = require('lowdb/node')
-const fs = require('fs')
+const { Low } = require('lowdb');
+const { JSONFile } = require('lowdb/node');
+const fs = require('fs');
 
-const adapter = new JSONFile('./database.json')
-const db = new Low(adapter)
+const adapter = new JSONFile('./database.json');
+const db = new Low(adapter);
 
-async function init() {
-  // If database.json does not exist, create it with default content
+async function initializeDB() {
   if (!fs.existsSync('./database.json')) {
-    fs.writeFileSync('./database.json', JSON.stringify({ toggles: {} }, null, 2))
+    fs.writeFileSync('./database.json', JSON.stringify({ toggles: {}, sessions: [] }, null, 2));
   }
 
-  await db.read()
-  db.data ||= { toggles: {} }
-  await db.write()
+  await db.read();
+  db.data ||= { toggles: {}, sessions: [] };
+  await db.write();
 }
 
-init()
+initializeDB(); // run setup but don't export promise
 
-module.exports = db
+module.exports = db;
