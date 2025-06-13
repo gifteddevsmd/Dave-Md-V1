8bc4b8ce368528a0865eb9717069a31e6ce9caab
gifteddaves <comradeanasafa@gmail.com>
2025-06-13 17:09:51 +0300

    Update db.js

--- db.js
+++ db.js
@@ -7,49 +7,8 @@ const db = new Low(adapter)
 async function init() {
   await db.read()
   db.data ||= {
-    users: {},
-    chats: {},
-    settings: {},
-    sessions: {},
-    statistic: {},
-    game: {},
-    others: {},
-    sticker: {},
-    dashboard: {},
-    message: {},
-    jadibot: {},
-    tos: {},
-    anonymous: {},
-    blockcmd: [],
-    cmdmedia: {},
-    queque: {},
-    premium: {},
-    name: {},
-    autobio: {},
-    ban: {},
-    badword: [],
-    viewOnce: [],
-    expired: [],
-    audio: {},
-    public: true,
-    restrict: false,
-    self: false,
-    autoreadsw: false,
-    autoblockcmd: false,
-    autoblok212: false,
-    gamewaktu: 60,
-    jadibotwaktu: 60,
-    debug: false,
-    auto: {
-      autoreactstatus: true,
-      autoreactmessages: true,
-      autosavestatus: true,
-      autoviewstatus: true
-    },
-    anti: {
-      antideletestatus: true
-    },
-    toggles: {}
+    toggles: {},
+    // your other default fields...
   }
   await db.write()
 }
