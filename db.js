741954cdaf14ff74d85c75163558d4bee5b67c06
gifteddaves <comradeanasafa@gmail.com>
2025-06-13 17:16:46 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,18 @@
 const { Low } = require('lowdb')
 const { JSONFile } = require('lowdb/node')
+const fs = require('fs')
 
 const adapter = new JSONFile('./database.json')
 const db = new Low(adapter)
 
 async function init() {
-  await db.read()
-  db.data ||= {
-    toggles: {},
-    // your other default fields...
+  // If database.json does not exist, create it with default content
+  if (!fs.existsSync('./database.json')) {
+    fs.writeFileSync('./database.json', JSON.stringify({ toggles: {} }, null, 2))
   }
+
+  await db.read()
+  db.data ||= { toggles: {} }
   await db.write()
 }
 
