c2e4705a222355da2f95c5adac45d6d347059b5e
gifteddaves <comradeanasafa@gmail.com>
2025-06-13 17:06:24 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,56 +1,59 @@
-import { Low } from 'lowdb'
-import { JSONFile } from 'lowdb/node'
+const { Low } = require('lowdb')
+const { JSONFile } = require('lowdb/node')
 
 const adapter = new JSONFile('./database.json')
 const db = new Low(adapter)
-await db.read()
 
-db.data ||= {
-  users: {},
-  chats: {},
-  settings: {},
-  sessions: {},
-  statistic: {},
-  game: {},
-  others: {},
-  sticker: {},
-  dashboard: {},
-  message: {},
-  jadibot: {},
-  tos: {},
-  anonymous: {},
-  blockcmd: [],
-  cmdmedia: {},
-  queque: {},
-  premium: {},
-  name: {},
-  autobio: {},
-  ban: {},
-  badword: [],
-  viewOnce: [],
-  expired: [],
-  audio: {},
-  public: true,
-  restrict: false,
-  self: false,
-  autoreadsw: false,
-  autoblockcmd: false,
-  autoblok212: false,
-  gamewaktu: 60,
-  jadibotwaktu: 60,
-  debug: false,
-  auto: {
-    autoreactstatus: true,
-    autoreactmessages: true,
-    autosavestatus: true,
-    autoviewstatus: true
-  },
-  anti: {
-    antideletestatus: true
-  },
-  toggles: {}
+async function init() {
+  await db.read()
+  db.data ||= {
+    users: {},
+    chats: {},
+    settings: {},
+    sessions: {},
+    statistic: {},
+    game: {},
+    others: {},
+    sticker: {},
+    dashboard: {},
+    message: {},
+    jadibot: {},
+    tos: {},
+    anonymous: {},
+    blockcmd: [],
+    cmdmedia: {},
+    queque: {},
+    premium: {},
+    name: {},
+    autobio: {},
+    ban: {},
+    badword: [],
+    viewOnce: [],
+    expired: [],
+    audio: {},
+    public: true,
+    restrict: false,
+    self: false,
+    autoreadsw: false,
+    autoblockcmd: false,
+    autoblok212: false,
+    gamewaktu: 60,
+    jadibotwaktu: 60,
+    debug: false,
+    auto: {
+      autoreactstatus: true,
+      autoreactmessages: true,
+      autosavestatus: true,
+      autoviewstatus: true
+    },
+    anti: {
+      antideletestatus: true
+    },
+    toggles: {}
+  }
+  await db.write()
 }
 
-await db.write()
+init()
 
-export default db
+module.exports = db
