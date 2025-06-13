9679b6ac23415d1355111d42207dca43646927cf
gifteddaves <comradeanasafa@gmail.com>
2025-06-13 05:26:07 +0300

    Create App.jsx

---
+++ session/frontend/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PairCodeGenerator from './components/PairCodeGenerator';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Gifted-Dave-MD - Pair Code Generator</h1>
+      <PairCodeGenerator />
+    </div>
+  );
+}
